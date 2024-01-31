e0ebecab1bc103f28c8e19d67404af7804428837
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-01-31 20:10:37 +0100

    update

--- app.js
+++ app.js
@@ -25,3 +25,38 @@ links.forEach((link) => {
 window.addEventListener('load', function (event) {
     event.preventDefault();
   });
+
+
+
+  const data = [
+    {
+        imageUrl: "./img/projects/database.svg",
+        title: "SQL Adatbázis",
+        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+    },
+    {
+        imageUrl: "./img/projects/design.svg",
+        title: "SQL Adatbázis",
+        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+    },
+    {
+        imageUrl: "./img/projects/mobileApp.svg",
+        title: "SQL Adatbázis",
+        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+    },
+    {
+        imageUrl: "./img/projects/react.svg",
+        title: "SQL Adatbázis",
+        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+    },
+    {
+        imageUrl: "./img/projects/static.svg",
+        title: "SQL Adatbázis",
+        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+    },
+    {
+        imageUrl: "./img/projects/static2.svg",
+        title: "SQL Adatbázis",
+        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+    },
+  ]
