b4c3ff43376edf87f22cfa43107b8bcf8f03c02c
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-02-02 18:31:38 +0100

    pordfolio card content

--- app.js
+++ app.js
@@ -24,39 +24,65 @@ links.forEach((link) => {
 
 window.addEventListener('load', function (event) {
     event.preventDefault();
-  });
+});
 
 
 
-  const data = [
+const datas = [
     {
         imageUrl: "./img/projects/database.svg",
+        alt: "database-icon",
         title: "SQL Adatbázis",
         text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
     },
     {
         imageUrl: "./img/projects/design.svg",
-        title: "SQL Adatbázis",
-        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+        alt: "Web design",
+        title: "Web design",
+        text: "A web design a digitális térben történő vizuális és funkcionális élmény megtervezésének művészete és tudománya.",
     },
     {
         imageUrl: "./img/projects/mobileApp.svg",
-        title: "SQL Adatbázis",
-        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+        alt: "mobil-App",
+        title: "Mobil applikáció",
+        text: "Mobil applikációk tervezése és kivitelezése",
     },
     {
         imageUrl: "./img/projects/react.svg",
-        title: "SQL Adatbázis",
-        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+        alt: "react-logo",
+        title: "React website",
+        text: "React keretrendszer ismerete és professzionális felhasználása.",
     },
     {
         imageUrl: "./img/projects/static.svg",
-        title: "SQL Adatbázis",
-        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+        alt: "icon",
+        title: "Statikus weboldalak",
+        text: "Statikus weboldalak tervezése és kivitelezése.",
     },
     {
         imageUrl: "./img/projects/static2.svg",
-        title: "SQL Adatbázis",
-        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+        alt: "icon",
+        title: "Wordpress oldalak",
+        text: "Wordpress weboldalak tervezése és kivitelezése.",
     },
-  ]
+]
+
+let container = document.querySelector('.cardContainer');
+
+datas.forEach(data => {
+    container.innerHTML +=
+        `
+    <div class="card">
+        <img class="image" src=${data.imageUrl} alt=${data.alt}>
+        <div class="contentWrapper">
+            <h2 class="title">${data.title}</h2>
+             <p class="text">${data.text}</p>
+             <div class="buttonGroup">
+                <a href="" class="btn">demo</a>
+                <a href="" class="btn">code</a>
+            </div>
+        </div>
+    </div>
+    `
+});
+
