c18db3d7e9248ee24519277ff199837b6d24b424
Thomas-Horvath <thomas.horvath0416@gmail.com>
2024-02-05 20:06:04 +0100

    I created data.js and imported datas in app.js file

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+import datas from './data.js';  // import datas of cards from other JavaScript file
+
 const button = document.querySelector('.menu');
 const navList = document.querySelectorAll('.js-navList');
 const links = document.querySelectorAll('.navLink');
 
-
-
+/* hamburger menu */
 button.addEventListener('click', () => {
     navList.forEach((e) => {
         e.classList.toggle('active');
     })
 });
-
-
 
 links.forEach((link) => {
     link.addEventListener('click', () => {
@@ -22,51 +21,8 @@ links.forEach((link) => {
 
 
 
-window.addEventListener('load', function (event) {
-    event.preventDefault();
-});
 
-
-
-const datas = [
-    {
-        imageUrl: "./img/projects/database.svg",
-        alt: "database-icon",
-        title: "SQL Adatbázis",
-        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
-    },
-    {
-        imageUrl: "./img/projects/design.svg",
-        alt: "Web design",
-        title: "Web design",
-        text: "A web design a digitális térben történő vizuális és funkcionális élmény megtervezésének művészete és tudománya.",
-    },
-    {
-        imageUrl: "./img/projects/mobileApp.svg",
-        alt: "mobil-App",
-        title: "Mobil applikáció",
-        text: "Mobil applikációk tervezése és kivitelezése",
-    },
-    {
-        imageUrl: "./img/projects/react.svg",
-        alt: "react-logo",
-        title: "React website",
-        text: "React keretrendszer ismerete és professzionális felhasználása.",
-    },
-    {
-        imageUrl: "./img/projects/static.svg",
-        alt: "icon",
-        title: "Statikus weboldalak",
-        text: "Statikus weboldalak tervezése és kivitelezése.",
-    },
-    {
-        imageUrl: "./img/projects/static2.svg",
-        alt: "icon",
-        title: "Wordpress oldalak",
-        text: "Wordpress weboldalak tervezése és kivitelezése.",
-    },
-]
-
+/*  cards render */
 let container = document.querySelector('.cardContainer');
 
 datas.forEach(data => {
---
+++ data.js
@@ -0,0 +1,40 @@
+const datas = [
+    {
+        imageUrl: "./img/projects/database.svg",
+        alt: "database-icon",
+        title: "SQL Adatbázis",
+        text: "Egy webshop teljes adatbázisa, termékkezeléssel, rendelés kezeléssel és a felhasználók kezelésével.",
+    },
+    {
+        imageUrl: "./img/projects/design.svg",
+        alt: "Web design",
+        title: "Web design",
+        text: "A web design a digitális térben történő vizuális és funkcionális élmény megtervezésének művészete és tudománya.",
+    },
+    {
+        imageUrl: "./img/projects/mobileApp.svg",
+        alt: "mobil-App",
+        title: "Mobil applikáció",
+        text: "Mobil applikációk tervezése és kivitelezése",
+    },
+    {
+        imageUrl: "./img/projects/react.svg",
+        alt: "react-logo",
+        title: "React website",
+        text: "React keretrendszer ismerete és professzionális felhasználása.",
+    },
+    {
+        imageUrl: "./img/projects/static.svg",
+        alt: "icon",
+        title: "Statikus weboldalak",
+        text: "Statikus weboldalak tervezése és kivitelezése.",
+    },
+    {
+        imageUrl: "./img/projects/static2.svg",
+        alt: "icon",
+        title: "Wordpress oldalak",
+        text: "Wordpress weboldalak tervezése és kivitelezése.",
+    },
+]
+
+export default datas;
